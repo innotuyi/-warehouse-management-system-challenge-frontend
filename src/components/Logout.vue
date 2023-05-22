5bdc38ecae80b7716f89e0e5cf23201bec4d4e1a
innotuyi <innotuyi67@gmail.com>
2023-05-22 11:43:17 +0200

    implementing logout functionality

---
+++ src/components/Logout.vue
@@ -0,0 +1,24 @@
+<template>
+ <H2>Logout</H2>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+     name: "LogoutView",
+  methods: {
+    async logout() {
+      try {
+        await axios.post('http://127.0.0.1:8000/api/logout');
+        this.$router.push('/');
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
