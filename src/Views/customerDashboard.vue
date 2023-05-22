ee2b2ab521e456a99327fbf6cdce87c628b19820
innotuyi <innotuyi67@gmail.com>
2023-05-22 11:41:35 +0200

    implementing customer dashboard

---
+++ src/Views/customerDashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <HeaderView />
+  </main>
+</template>
+
+<script>
+import HeaderView from '../components/Header.vue'
+
+export default {
+  name: "customerDashboard",
+  components: {
+    HeaderView,
+  },
+};
+</script>
+
+<style>
+@import "../assets/css/style.css";
+</style>
